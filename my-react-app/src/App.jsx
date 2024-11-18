773f5d891bf26c7cae2e2ce6f20312c1cd68f2e6
jiths3 <jithinchacko71@gmail.com>
2024-11-18 15:34:59 +0530

    using js obj while returning{}

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./Navbar.jsx";
+import Food from "./Food.jsx";
 
 export default function App() {
   
-  return <Navbar />
+  return (
+        <>
+          <Navbar />
+          <Food />
+        </>  
+  )
     
-
-  
-  
-
 }
 
---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,14 @@
+export default function Food(){
+
+    const fruit1 = "Apple"
+    const fruit2 = "Mango"
+    
+    return(
+        <ul>
+            <li>orange</li>
+            <li>{fruit1}</li>
+            <li>{fruit2}</li>
+        </ul>
+
+    );
+}
