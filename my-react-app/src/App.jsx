61ef40a23a118059abea0bf6e9eb29fcf376a26d
jiths3 <jithinchacko71@gmail.com>
2024-12-09 22:31:44 +0530

    array updation using state

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -20,6 +20,7 @@ return(
     <ColorPicker />
     <Counter />
     <Updater />
+    <List />
    
 
 
--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,16 +1,35 @@
 import React, { useState } from 'react'
 
 
-export default function List(props) {
-
-   
-    const ItemsList = props.items               
-    const ListItems = ItemsList.map(item=> <li>{item.name}{item.calories}</li> )
+export default function List() {
 
 
-    return(<>   
+    const[foods,setfoods] = useState(["orange", "apple", "pinepple"]) 
 
-            <ol>{ListItems}</ol>
+    function AddFood () {
+        const newFood = document.getElementById("foodInput").value
+        setfoods(f=> [...f , newFood])
+        document.getElementById("foodInput").value = "" ;
+    }
+
+    function deleteFood (index) {
+        setfoods(foods.filter((_,i) => i !== index ));
+        
+    }
+
+    return(<>
+
+
+        <h1>List of foods</h1>
+        <ol> 
+            {foods.map((food,index) => <li key={index} onclick={() => deleteFood(index)}>
+            {food}
+            </li>)}    
+        </ol>
+        <input type="text" id="foodInput" />
+        <button onClick={AddFood}>add food</button>
+
+                         
 
 
      </>);
